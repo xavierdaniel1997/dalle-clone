f8ac55288dd4f16136ef689ccb7432be5e3c9df1
Daniel Xavier <danielcx@gmail.com>
2023-03-09 21:15:27 +0530

    completed server sections

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));  
 
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
